ea6fb57cb3de9904b0d4f8abe9817d8289515255
Malixa <auligan44@gmail.com>
2016-12-26 15:53:20 +0100

    Reecriture du client ajout de la grille

--- Client/client.py
+++ Client/client.py
@@ -4,7 +4,7 @@ import socket
 import select
 import sys
 import re
-import ../Src/Serveur/Game/grid
+from . import grid as grid
 
 TURN = re.compile('^TURN')
 OK = re.compile('^OK')
@@ -22,15 +22,34 @@ GETSCORE = "GETSCORE"
 EXIT = "EXIT"
 PLACE = "PLACE"
 
+
+""" 
+on entre l'ip et le port du serveur 
+"""
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-sock.connect(('',argv[1]))
+sock.connect((argv[1],argv[2])) 
 
 sock.send(CONNECT.encode("utf-8"))
+
 data = sock.recv(1024)
 new_data = data.decode("utf-8")
-if re.match(NOP, new_data):
+
+
+""" 
+si la connexion échoue 
+"""
+
+def fail() :
     print("Invalid connection (error server / already connected)")
-elif re.match(PLAYER, new_data):
+
+
+
+
+""" 
+si on est un joueur
+"""
+def player (data) :
     print("Vous êtes un joueur")
     num = new_data.split(" ")
     sock.send(GETSTATE.encode("utf-8"))
@@ -38,45 +57,68 @@ elif re.match(PLAYER, new_data):
     new_data = data.decode("utf-8")
     tab = new_data.split(" ")
     tab = tab[1].split(",")
-    __init__(tab)
+	for i in tab:
+    	grid.place(self,num,i)
+	grid.display(self)
     data = sock.recv(1024)
     new_data = data.decode("utf-8")
-    if re.match(TURN, new_data)
+
+    if re.match(TURN, new_data):
         place = -1
-        while re.match(OK, new_data)==false
+
+        while re.match(OK, new_data)==false :
+
             while place <0 or place >8
                 print("placer votre pion")
                 place = input()
-                if place = "exit"
+
+                if place = "exit" :
                     sock.send("EXIT")
+
             sock.send("PLACE ",place)
             data = sock.recv(1024)
             new_data = data.decode("utf-8")
-            if re.match(OK, new_data)
+
+            if re.match(OK, new_data) :
                 print("a l'autre joueur")
-            else
+
+            else :
                 print("cette case est déjà occupée, replacer votre pion")
-                
-else
+ 
+
+
+
+
+"""
+si on est un observateur
+"""             
+def obs(data):
     print("Vous êtes un observateur")
     sock.send(GETSCORE.encode("utf-8"))
     data = sock.recv(1024)
     new_data = data.decode("utf-8")
-    if re.match(SCORE,new_data)
+
+    if re.match(SCORE,new_data) :
         tab = new_data.split(',')
         print("Voici le score des 2 joueurs : ",tab[0]," / ",tab[1])
-        while true
+
+        while true :
             sock.send(GETSTATE.encode("utf-8"))
             data = sock.recv(1024)
             new_data = data.decode("utf-8")
-            if re.match(STATE,new_data)
+
+            if re.match(STATE,new_data) :
                 tab = new_data.split(',')
-                __init__(tab[1..9])
+                for i in tab[1..8]:
+    				grid.place(self,0,i)
+					grid.place(self,1,i)
+				grid.display(self)
             
             
         
             
-            
+def main()
+	            
 
 
        
